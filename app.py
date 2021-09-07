fb969a39d826956c6d9a64abd3c976aca031e0eb
equable23025 <ni.siraprapa@gmail.com>
2021-09-07 21:50:49 +0700

    Update app.py
    test2

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+#!pip install --quiet jupyter-dash pyngrok
+
+#import dash 
+from jupyter_dash import JupyterDash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from pyngrok import ngrok
+import pandas as pd
+import dash
+from dash.dependencies import Input, Output
+
+
+app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
+
+if __name__ == '__main__':
+  
+  app.run_server(debug=True)
 
